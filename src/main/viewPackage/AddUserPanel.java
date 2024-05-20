038f5d4793b971574ab165eaa260505ae52fd228
TeoTayenne <etu52276@henallux.be>
2024-05-21 01:48:34 +0200

    Revert "Merge remote-tracking branch 'upstream/HEAD' into main1"
    This reverts commit 7f6cbb0674189976cd17ea12bafeb0cc7d350074, reversing
    changes made to 08083a4c3f4298fc7fc6d190d97a9d01de92f174.

--- src/main/viewPackage/AddUserPanel.java
+++ src/main/viewPackage/AddUserPanel.java
@@ -4,152 +4,82 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
-import java.sql.Date;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
 
 import main.controllerPackage.CountriesController;
-import main.controllerPackage.UserController;
 import main.exceptionPackage.ConnectionDataAccessException;
 import main.exceptionPackage.CountriesDAOException;
-import main.exceptionPackage.LocalityException;
-import main.modelPackage.LocalityModel;
 
-public class AddUserPanel extends JPanel implements ActionListener, ItemListener {
+public class AddUserPanel extends JPanel implements ActionListener {
     private String[] GENDER_CHOICE = {"Male", "Female", "Other"};
     private int TEXT_FIELD_COLUMNS = 20;
-    private int LABEL_PADDING = 20;
-    private int FIELD_PADDING = 5;
 
-    private MainWindow mainWindow;
-    private int nbFields = 0;
-    private JLabel topLabel;
     private JTextField username;
-    private JPasswordField password;
+    private JTextField password;
     private JTextField email;
     private JTextField dateOfBirth;
     private JTextField phoneNumber;
     private JTextField street;
     private JTextField bio;
     private JCheckBox isAdmin;
-    private JSpinner dateOfBirthSpinner;
-    private SpinnerDateModel dateOfBirthModel;
     private JButton submit;
-    private JComboBox<String> zipCode;
     private JComboBox<String> country;
     private JComboBox<String> gender;
     private Dimension textFieldSize = new JTextField(TEXT_FIELD_COLUMNS).getPreferredSize();
 
     private CountriesController countriesController;
-    private UserController userController;
 
-    public AddUserPanel(MainWindow mainWindow) throws CountriesDAOException, ConnectionDataAccessException, LocalityException {
+    public AddUserPanel() throws CountriesDAOException, ConnectionDataAccessException {
         countriesController = new CountriesController();
-        userController = new UserController();
-
-        this.mainWindow = mainWindow;
 
         setLayout(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
-        gbc.insets = new Insets(LABEL_PADDING, LABEL_PADDING, LABEL_PADDING, LABEL_PADDING);
+        gbc.insets = new Insets(5, 5, 5, 5);  // Padding between components
 
-        topLabel = new JLabel("Création d'un utilisateur");
+        JLabel label = new JLabel("Add User Panel");
         gbc.gridx = 0;
-        gbc.gridy = nbFields;
+        gbc.gridy = 0;
         gbc.gridwidth = 2;
         gbc.anchor = GridBagConstraints.CENTER;
-        add(topLabel, gbc);
+        add(label, gbc);
 
-        gbc.insets = new Insets(FIELD_PADDING, FIELD_PADDING, FIELD_PADDING, FIELD_PADDING);
         gbc.gridwidth = 1;
         gbc.anchor = GridBagConstraints.WEST;
-        addField(gbc, "Champs Obligatoires *", new JLabel(""));
-        addField(gbc, "Email *", email = new JTextField(TEXT_FIELD_COLUMNS));
-        addField(gbc, "Nom d'utilisateur *", username = new JTextField(TEXT_FIELD_COLUMNS));
-        addField(gbc, "Mot de passe *", password = new JPasswordField(TEXT_FIELD_COLUMNS));
-
-        dateOfBirthModel = new SpinnerDateModel();
-        dateOfBirthSpinner = new JSpinner(dateOfBirthModel);
-        dateOfBirthSpinner.setEditor(new JSpinner.DateEditor(dateOfBirthSpinner, "dd/MM/yyyy"));
-        dateOfBirthSpinner.setPreferredSize(textFieldSize);
-
-        addField(gbc, "Date de naissance *", dateOfBirthSpinner);
-        addField(gbc, "Rue et numéro *", street = new JTextField(TEXT_FIELD_COLUMNS));
-
-        zipCode = new JComboBox<>();
-        zipCode.setPreferredSize(textFieldSize);
-        addField(gbc, "Code postal *", zipCode);
-
-        addField(gbc, "Téléphone", phoneNumber = new JTextField(TEXT_FIELD_COLUMNS));
-        addField(gbc, "Biographie", bio = new JTextField(TEXT_FIELD_COLUMNS));
+        addField(gbc, "Email", email = new JTextField(TEXT_FIELD_COLUMNS), 1);
+        addField(gbc, "Username", username = new JTextField(TEXT_FIELD_COLUMNS), 2);
+        addField(gbc, "Password", password = new JPasswordField(TEXT_FIELD_COLUMNS), 3);
+        addField(gbc, "Date of Birth", dateOfBirth = new JTextField(TEXT_FIELD_COLUMNS), 4);
+        addField(gbc, "Street and number", street = new JTextField(TEXT_FIELD_COLUMNS), 5);
+        addField(gbc, "Phone Number", phoneNumber = new JTextField(TEXT_FIELD_COLUMNS), 6);
+        addField(gbc, "Biography", bio = new JTextField(TEXT_FIELD_COLUMNS), 7);
 
         gender = new JComboBox<>(GENDER_CHOICE);
         gender.setPreferredSize(textFieldSize);
-        addField(gbc, "Genre *", gender);
+        addField(gbc, "Gender", gender, 8);
 
         country = new JComboBox<>(countriesController.getCountries().toArray(new String[0]));
         country.setPreferredSize(textFieldSize);
-        country.addItemListener(this);
-        addField(gbc, "Pays *", country);
-        refreshLocalities();
+        addField(gbc, "Country", country, 9);
 
-        addField(gbc, "Admin *", isAdmin = new JCheckBox());
+        addField(gbc, "Admin user", isAdmin = new JCheckBox(), 10);
 
         submit = new JButton("Ajouter l'utilisateur");
         submit.setPreferredSize(textFieldSize);
-        submit.addActionListener(this);
-        addField(gbc, "", submit);
+        addField(gbc, "", submit, 11);
 
         setVisible(true);
     }
 
-    private void addField(GridBagConstraints gbc, String labelText, Component component) {
-        nbFields++;
+    private void addField(GridBagConstraints gbc, String labelText, Component component, int yPos) {
         gbc.gridx = 0; // Texte a gauche
-        gbc.gridy = nbFields; // Position du component
+        gbc.gridy = yPos; // Position du component
         add(new JLabel(labelText), gbc);
 
         gbc.gridx = 1; // placement du component a droite du texte correspondant
         add(component, gbc);
     }
 
-    private void refreshLocalities() {
-        List<LocalityModel> localities;
-        try {
-            localities = userController.getLocality(Objects.requireNonNull(country.getSelectedItem()).toString());
-            zipCode.removeAllItems();
-            List<String> formattedLocalities = new ArrayList<>();
-            if (localities.isEmpty()) {
-                formattedLocalities.add("Aucune localité trouvée");
-            } else {
-                for (LocalityModel locality : localities) {
-                    formattedLocalities.add(locality.getCity() + " - " + locality.getZipCode());
-                }
-            }
-
-            for (String formattedLocality : formattedLocalities) {
-                zipCode.addItem(formattedLocality);
-            }
-        } catch (ConnectionDataAccessException | LocalityException ex) {
-            mainWindow.displayError(ex.toString());
-        }
-    }
-
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == submit) {
-            Date date = new Date(dateOfBirthModel.getDate().getTime());
-            // send sql via controller
-        }
-    }
 
-    @Override
-    public void itemStateChanged(ItemEvent e) {
-        if (e.getSource() == country) {
-            refreshLocalities();
-        }
     }
 }
